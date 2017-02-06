de20f6e9ef194dd734013be65a1c87fa8027f31c
n0z0n <mail+github@n0z.me>
2017-02-06 14:46:05 +0900

    Modify initial script and rcrc.

--- init.sh
+++ init.sh
@@ -2,10 +2,8 @@
 
 export GOPATH=~/.local/
 git config --global ghq.root "~/.local/src"
-
 go get github.com/motemen/ghq
 ~/.local/bin/ghq get n0z0n/dotfiles
-ln -s `$GOPATH/bin/ghq list -p | grep n0z0n` ~/.dotfiles
 
 cd `$GOPATH/bin/ghq list -p | grep n0z0n`
 git flow init -d
@@ -13,9 +11,9 @@ git flow feature track scratch
 git submodule update --init
 cd ~
 
-~/.local/bin/ghq get yyuu/pyenv
-
+${GOPATH}/bin/ghq get yyuu/pyenv
 ln -s `$GOPATH/bin/ghq list -p | grep pyenv` ~/.pyenv
+ln -s `$GOPATH/bin/ghq list -p | grep n0z0n`/rcrc ~/.rcrc
 
 rcup
 zsh -c vim

d8a0cbc9f5b6d9f119c42f12ced6ae0513baef0a
n0z0n <mail+github@n0z.me>
2018-04-05 14:39:34 +0000

    Move direnv hook after set environment variables.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,5 @@
+set -x PATH      /home/linuxbrew/.linuxbrew/bin  $PATH
+set -x MANPATH   (brew --prefix)/share/man:      $MANPATH
+set -x INFOPATH  (brew --prefix)/share/info:     $INFOPATH
+
 eval (direnv hook fish)
-set -x fish_path .ghq/github.com/n0z0n/dotfiles/.config/fish

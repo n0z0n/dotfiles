a0a78c2a9e49f05417e017d727a7bd1452533523
n0z0n <mail+github@n0z.me>
2018-05-16 14:43:43 +0900

    Modify brew path.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
-set -x PATH      /home/linuxbrew/.linuxbrew/bin  $PATH
+set -x PATH      ~/.linuxbrew/bin  $PATH
 set -x MANPATH   (brew --prefix)/share/man:      $MANPATH
 set -x INFOPATH  (brew --prefix)/share/info:     $INFOPATH
 
 eval (direnv hook fish)
-set -x fish_path .ghq/github.com/n0z0n/dotfiles/.config/fish
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
